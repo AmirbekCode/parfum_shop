14be083db8827f006de3299e5690ed0df5d6dcb1
zairov01 <106994015+AmirbekCode@users.noreply.github.com>
2023-03-23 16:30:02 +0500

    card2

--- src/components/Carausel/Carausel.jsx
+++ src/components/Carausel/Carausel.jsx
@@ -7,22 +7,22 @@ function IndividualIntervalsExample() {
       <Carousel.Item interval={1000}>
         <img
           className="d-block w-100"
-          src={Card}
+          src={Card2}
           alt="First slide"
         />
       </Carousel.Item>
       <Carousel.Item interval={500}>
         <img
           className="d-block w-100"
-          src={Card}
+          src={Card2}
           alt="Second slide"
         />
-
+  
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src={Card}
+          src={Card2}
           alt="Third slide"
         />
       </Carousel.Item>
